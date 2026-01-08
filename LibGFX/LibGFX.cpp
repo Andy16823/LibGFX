89a00defbe47e551d35289983c39bdaa67faefac
Andreas Wagner <andy16823@live.de>
2026-01-08 20:34:23 +0100

    Projektdateien hinzufügen.

---
+++ LibGFX/LibGFX.cpp
@@ -0,0 +1,16 @@
+﻿// LibGFX.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+#include "LibGFX.h"
+
+using namespace std;
+
+
+GLFWwindow* CreateContext(int width, int height, const char* title)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	auto window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+	return window;
+}
---
+++ LibGFX/LibGFX.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+GLFWwindow* CreateContext(int width, int height, const char* title);
