38ea3b4be370e3445dadaf33803f35941d45edf2
Andreas Wagner <andy16823@live.de>
2026-01-09 19:00:43 +0100

    Standard-Konstruktor und -Destruktor zu RenderPass hinzugefügt
    Standard-Konstruktor und virtueller Standard-Destruktor wurden der RenderPass-Klasse hinzugefügt, um eine korrekte Konstruktion und Zerstörung, insbesondere bei Vererbung, sicherzustellen.

--- LibGFX/RenderPass.h
+++ LibGFX/RenderPass.h
@@ -4,6 +4,8 @@
 namespace LibGFX {
 	class RenderPass {
 		public:
+			RenderPass() = default;
+			virtual ~RenderPass() = default;
 			virtual VkRenderPass getRenderPass() const = 0;
 			virtual bool create(VkDevice device, VkFormat swapchainImageFormat, VkFormat depthFormat) = 0;
 			virtual void destroy(VkDevice device) = 0;
