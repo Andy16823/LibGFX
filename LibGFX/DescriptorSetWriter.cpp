eae06f7dc4e1806283fe5219fed5e4f842ab32d0
Andreas Wagner <andy16823@live.de>
2026-01-10 19:52:14 +0100

    DescriptorSetWriter: API vereinfacht, Bindings als Parameter
    Die Methoden setDstBinding, setDstArrayElement und setDescriptorType wurden entfernt. Deren Werte werden nun direkt als Parameter an write übergeben, das zudem einen Verweis auf das Objekt zurückgibt. Die zugehörigen Membervariablen wurden entfernt und clear setzt nur noch die Buffer-Infos zurück. Dies vereinfacht die Nutzung und macht die Klasse flexibler.

--- LibGFX/DescriptorSetWriter.cpp
+++ LibGFX/DescriptorSetWriter.cpp
@@ -10,42 +10,22 @@ LibGFX::DescriptorSetWriter& LibGFX::DescriptorSetWriter::addBufferInfo(VkBuffer
 	return *this;
 }
 
-LibGFX::DescriptorSetWriter& LibGFX::DescriptorSetWriter::setDstBinding(uint32_t dstBinding)
-{
-	m_dstBinding = dstBinding;
-	return *this;
-}
-
-LibGFX::DescriptorSetWriter& LibGFX::DescriptorSetWriter::setDstArrayElement(uint32_t dstArrayElement)
-{
-	m_dstArrayElement = dstArrayElement;
-	return *this;
-}
-
-LibGFX::DescriptorSetWriter& LibGFX::DescriptorSetWriter::setDescriptorType(VkDescriptorType descriptorType)
-{
-	m_descriptorType = descriptorType;
-	return *this;
-}
-
-void LibGFX::DescriptorSetWriter::write(VkContext& context, VkDescriptorSet descriptorSet)
+LibGFX::DescriptorSetWriter& LibGFX::DescriptorSetWriter::write(VkContext& context, VkDescriptorSet descriptorSet, uint32_t dstBinding, uint32_t dstArrayElement, VkDescriptorType descriptorType)
 {
 	VkWriteDescriptorSet writeInfo = {};
 	writeInfo.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
 	writeInfo.dstSet = descriptorSet;
-	writeInfo.dstBinding = m_dstBinding;
-	writeInfo.dstArrayElement = m_dstArrayElement;
+	writeInfo.dstBinding = dstBinding;
+	writeInfo.dstArrayElement = dstArrayElement;
 	writeInfo.descriptorCount = 1;
-	writeInfo.descriptorType = m_descriptorType;
+	writeInfo.descriptorType = descriptorType;
 	writeInfo.pBufferInfo = m_bufferInfos.data();
 
 	vkUpdateDescriptorSets(context.getDevice(), 1, &writeInfo, 0, nullptr);
+	return *this;
 }
 
 void LibGFX::DescriptorSetWriter::clear()
 {
 	m_bufferInfos.clear();
-	m_dstBinding = 0;
-	m_dstArrayElement = 0;
-	m_descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 }
--- LibGFX/DescriptorSetWriter.h
+++ LibGFX/DescriptorSetWriter.h
@@ -8,15 +8,9 @@ namespace LibGFX {
 	{
 	public:
 		DescriptorSetWriter& addBufferInfo(VkBuffer buffer, VkDeviceSize offset, VkDeviceSize range);
-		DescriptorSetWriter& setDstBinding(uint32_t dstBinding);
-		DescriptorSetWriter& setDstArrayElement(uint32_t dstArrayElement);
-		DescriptorSetWriter& setDescriptorType(VkDescriptorType descriptorType);
-		void write(VkContext& context, VkDescriptorSet descriptorSet);
+		DescriptorSetWriter& write(VkContext& context, VkDescriptorSet descriptorSet, uint32_t dstBinding, uint32_t dstArrayElement, VkDescriptorType descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
 		void clear();
 	private:
 		std::vector<VkDescriptorBufferInfo> m_bufferInfos;
-		uint32_t m_dstBinding = 0;
-		uint32_t m_dstArrayElement = 0;
-		VkDescriptorType m_descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 	};
 }
