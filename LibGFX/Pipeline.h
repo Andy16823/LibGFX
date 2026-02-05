8a1cb62796fa3f35f72a0ac518e9237ab1f194ca
Andreas Wagner <andy16823@live.de>
2026-02-05 18:02:06 +0100

    Add VkContext forward-decl and lifecycle methods
    Forward-declare VkContext to avoid a circular dependency and extend the Pipeline interface with pure virtual lifecycle methods: create(VkContext*, VkRenderPass) and destroy(VkContext*). This allows concrete pipeline implementations to be created and destroyed with access to a VkContext and a VkRenderPass.

--- LibGFX/Pipeline.h
+++ LibGFX/Pipeline.h
@@ -2,6 +2,10 @@
 #include <vulkan/vulkan.h>
 
 namespace LibGFX {
+
+	// Forward declaration of VkContext to avoid circular dependency
+	class VkContext;
+
 	// Abstract base class for a graphics pipeline
 	class Pipeline {
 
@@ -9,6 +13,8 @@ namespace LibGFX {
 		Pipeline() = default;
 		virtual ~Pipeline() = default;
 
+		virtual void create(VkContext* context, VkRenderPass renderPass) = 0;
+		virtual void destroy(VkContext* context) = 0;
 		virtual VkPipeline getPipeline() const = 0;
 		virtual VkPipelineLayout getPipelineLayout() const = 0;
 	};
